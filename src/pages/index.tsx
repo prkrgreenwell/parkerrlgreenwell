5d660aed953b0f5929b70aa5bc1ce532ba05c130
Parker Greenwell <pgreenwell@asd.alpinedistrict.org>
2023-12-18 14:57:08 -0700

    started styling

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Head from "next/head";
+import NavBar from "@/components/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Parker Greenwell | Full-Stack Developer</title>
+      </Head>
+      <NavBar />
+    </div>
+  );
+}
